d98282de644a130ff75381c0918eefd923d2f28d
kal93 <maaz2dt@gmail.com>
2019-09-13 17:38:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# heartbeat
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/heartbeat)
---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Subscription, interval } from 'rxjs';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit, OnDestroy  {
+  name = 'Heartbeat';
+
+  heartBeatSubscription : Subscription;
+
+  constructor(protected heartBeatHttp: HttpClient) {
+
+  }
+
+   ngOnInit() {
+
+    // change 2000 to whatever number
+    const observable = interval(5000);
+    this.heartBeatSubscription  = observable.pipe(
+      // see if flatMap can be used to remove the nested subscription
+      // flatMap(() = > { this.callHeartBeat())
+    )
+    .subscribe(
+      intervalResponse => {
+        console.log(intervalResponse);
+        this.callHeartBeat().subscribe( (heartBeatResponse) => {
+          console.log(heartBeatResponse);
+        }
+        );
+      },
+      error => {
+        console.log('heartbeat error');
+        console.log(error);
+        if (error instanceof HttpErrorResponse) {
+          alert(error.status + ' ' + error.statusText);
+        }
+      },
+      () => {
+        console.log('complete');
+      }
+    );
+  }
+  callHeartBeat() {
+    let hash = 'SU1fQURNSU46a1N6U1JaUWd4URZSRVNUU0VSVkVSQ0FMTA==';
+    let headers : HttpHeaders = new HttpHeaders();
+    headers.append('Authorization', `Basic ${hash}`);
+    return this.heartBeatHttp
+      .post('http://localhost:8000/csp/imrestbb/v1/session/heartbeat', '', { headers : headers});
+  }
+
+  logout() {
+    console.log('kill heartbeat');
+    this.heartBeatSubscription.unsubscribe();
+  }
+
+  ngOnDestroy() {
+     this.heartBeatSubscription.unsubscribe();
+  }
+}
